c9d1e660428262c7dd06d8e2ce5c7425e654ae77
Jasmine Davis <jasminedavis@new-host-4.home>
2020-11-11 16:55:10 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Welcome to Daily Blog
+
+This daily blog counts your words as you write them, then offers handy statistics about the amount of words you've written.
+
+## To Run
+Clone & visit localhost:3000/blogs/ in your browser.
+
---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,29 @@
+class BlogsController < ApplicationController
+  def index
+    @blogs = Blog.all
+  end
+
+  def new
+  end
+  
+  def create
+    my_blog = Blog.new
+    my_blog.title = params["title"]
+    my_blog.text = params["text"]
+    my_blog.wordcount = params["wordcount"]
+    my_blog.save
+    redirect_to blogs_path
+  end
+
+  def edit
+  end
+
+  def show
+    #should pull the blog from the id
+    @blogs = Blog.find(params[:id])
+  end
+  private
+  def blogs_params
+    params.require(:blogs).permit(:title,:text,:wordcount)
+  end
+end
